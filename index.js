fb3b776ff4265ac82ddca622d1a7f64ef4a550b7
Christopher <christopher.black.2209@googlemail.com>
2021-12-05 01:35:18 +0000

    commit tree 8c59d2c6beb4e9f786cbd38229f7af76219ac78d

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Add your functions here
+function map(sourceArray, callbackFunction) {
+    let result = [];
+    for (const element of sourceArray) {
+        result.push(callbackFunction(element));
+    }
+    return result
+}
+
+function reduce(sourceArray, callbackFunction, startValue) {
+    if (startValue) {
+        let aggregator = startValue;
+        for (const element of sourceArray) {
+            aggregator = callbackFunction(element, aggregator)
+        }
+        return aggregator
+    } else {
+        let aggregator = sourceArray[0];
+        for (let i = 1; i < sourceArray.length; i++) {
+            aggregator = callbackFunction(sourceArray[i], aggregator)  
+        }
+        return aggregator
+    }   
+}
+
+// function reduceToTotal(sourceArray, startingPoint = 0) {
+//     let sum = startingPoint;
+//     for (const element of sourceArray) {
+//         sum += element
+//     }
+//     return sum
+// }
+
+// function reduceToAllTrue(sourceArray) {
+//     let result = false;
+//     for (const element of sourceArray) {
+//         if (!!element === true) {
+//             result = true
+//         }
+//         else {
+//             result = false
+//         }
+//     }
+//     return result
+// }
+
+// function reduceToAnyTrue(sourceArray) {
+//     let result = false;
+//     for (const element of sourceArray) {
+//         if (!!element === true) {
+//             result = true
+//         }
+//     }
+//     return result
+// }
